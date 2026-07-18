25c3b848f66a387208fcbe8ad3b4fae561abd187
Giantte Chambers <gchamb.dev@gmail.com>
2026-07-18 12:29:27 -0500

    infra: create a bucket

---
+++ infra/environments/prod/main.tf
@@ -0,0 +1,4 @@
+module "application_bucket" {
+  source = "../../modules/s3"
+  name   = "catholic-rag"
+}
---
+++ infra/modules/s3/variables.tf
@@ -0,0 +1,4 @@
+variable "name" {
+  description = "The name of the s3 bucket"
+  type        = string
+}
