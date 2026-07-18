5b6e15da9a90e73a1f47d0d53a35d55d64b5e8a3
Giantte Chambers <gchamb.dev@gmail.com>
2026-07-18 12:42:25 -0500

    The document table should have a status separate from the chunking jobs

--- packages/db/src/drizzle-instance.ts
+++ packages/db/src/drizzle-instance.ts
@@ -1,8 +1,10 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { drizzle, type NodePgDatabase } from 'drizzle-orm/node-postgres';
+import { relations } from "./relations.js"
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not set');
 }
 
-export const db = drizzle({ connection: process.env.DATABASE_URL });
+export const db: NodePgDatabase = drizzle(process.env.DATABASE_URL!, { relations });
+
 export default db;
--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,13 +1,18 @@
 import { uuid, pgTable, timestamp, text, pgEnum, jsonb, integer, vector } from "drizzle-orm/pg-core";
 
+export const documentStatusEnum = pgEnum("document_status_enum", [
+  'awaiting_approval',
+  'approved',
+  // no denied, because if denied we are deleting it.
+]);
+
 export const documentTable = pgTable("document", {
   id: uuid("id").primaryKey().defaultRandom(),
   fileKey: text("file_key").notNull(),
+  status: documentStatusEnum("status").notNull().default("awaiting_approval"),
   createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
 });
-
 export const chunkingJobEnum = pgEnum("chunking_job_status_enum", [
-  'awaiting_approval',
   'scheduled',
   'running',
   'succeeded',
@@ -16,8 +21,8 @@ export const chunkingJobEnum = pgEnum("chunking_job_status_enum", [
 
 export const chunkingJobTable = pgTable("chunking_job", {
   id: uuid("id").primaryKey().defaultRandom(),
-  documentId: uuid("document_id").notNull(),
-  status: chunkingJobEnum("status").notNull().default("awaiting_approval"),
+  documentId: uuid("document_id").notNull().references(() => documentTable.id, { onDelete: "cascade" }),
+  status: chunkingJobEnum("status").notNull().default("scheduled"),
   createdAt: timestamp("created_at", { withTimezone: true }),
   approvedAt: timestamp("approved_at", { withTimezone: true }),
   startedAt: timestamp("started_at", { withTimezone: true }),
@@ -27,7 +32,7 @@ export const chunkingJobTable = pgTable("chunking_job", {
 
 export const chunks = pgTable("chunks", {
   id: uuid("id").primaryKey().defaultRandom(),
-  documentId: uuid("document_id").notNull(),
+  documentId: uuid("document_id").notNull().references(() => documentTable.id, { onDelete: "cascade" }),
   content: text("content").notNull(),
   chunkIndex: integer("chunk_index").notNull(),
   embedding: vector({
